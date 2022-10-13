4ef2bccfcdd25a30be9b49d29f7944f1e777ed94
David <Rigby335@hotmail.com>
2022-10-13 11:49:27 -0700

    added custom components for form

--- src/components/CustomForms/FormControl.jsx
+++ src/components/CustomForms/FormControl.jsx
@@ -1,13 +1,66 @@
-// build custom components here
+/* eslint-disable react/prop-types */
+import classnames from 'classnames';
+import styles from './FormControl.css';
 
-// input
-// type="file"
+function FormControl({ label, children, required }) {
+  const className = classnames(styles.FormControl, styles.LabelText);
+  return (
+    <label className={className}>
+      <LabelText text={label} required={required} />
+      {children}
+    </label>
+  );
+}
 
-// formButton
+export function LabelText({ text, required }) {
+  const className = classnames(styles.LabelText, {
+    [styles.Required]: required,
+  });
+  return <span className={className}>{text}</span>;
+}
 
-// selectForm
+export function InputControl({ label, required, ...rest }) {
+  return (
+    <FormControl label={label} required={required} >
+      <input {...rest} required={required}/>
+    </FormControl>
+  );
+}
 
-// image URL input
+export function CheckboxControl({ legend, required, label, ...rest }) {
+  return (
+    <fieldset className={styles.CheckboxControl}>
+      <legend >
+        <LabelText text={legend} required={required}/>
+      </legend>
+      <label >
+        <input type="checkbox" required={required} {...rest}/>
+        {label}
+      </label>
+    </fieldset>
+  );
+}
 
-// text auth input
+export function SelectControl({ 
+  label, 
+  required, 
+  placeholder, 
+  children, 
+  ...rest }) {
+  return (
+    <FormControl label={label} required={required}>
+      <select {...rest} required={required}>
+        {placeholder && <option disabled>{placeholder}</option>}
+        {children}
+      </select>
+    </FormControl>
+  );
+}
 
+export function FormButton({ children, ...rest }) {
+  return (
+    <button className={styles.FormButton} {...rest}>
+      {children}
+    </button>
+  );
+}
