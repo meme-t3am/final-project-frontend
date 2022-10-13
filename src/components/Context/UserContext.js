c0adc4e90cf51172dbc8f0f4e28acedc3c21c1a8
‘AMAYA <aamaya547@gmail.com>
2022-10-13 14:37:58 -0700

    Added UserContext to manage user state and user data

--- src/components/Context/UserContext.js
+++ src/components/Context/UserContext.js
@@ -1,3 +1,35 @@
 // verify user and set user state
 
-// useContext()
+import { createContext, useContext, useEffect, useState } from 'react';
+import { verifyUser } from '../Services/auth';
+
+const UserContext = createContext();
+
+export default function UserProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  const verify = async () => {
+    const res = await verifyUser();
+    res.status != 401 ? setUser(res.user) : null;
+  };
+
+  useEffect(() => {
+    verify();
+  }, []);
+
+  const stateAndSetters = {
+    user,
+    setUser,
+  };
+
+  return (
+    <UserContext.Provider value={stateAndSetters}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUser() {
+  const { user, setUser } = useContext(UserContext);
+  return [user, setUser];
+}
