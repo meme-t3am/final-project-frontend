874819803cc143587c3076f81d49827001baea1d
‘AMAYA <aamaya547@gmail.com>
2022-10-13 11:56:41 -0700

    Added proxys to webpack

--- src/components/Services/auth.js
+++ src/components/Services/auth.js
@@ -1,1 +1,10 @@
+import { post } from './request';
+
+const BASE_URL = '/api/v1/users';
 // do fetches to backend users table here
+export async function signUpUser(credentials) {
+  const res = await post(`${BASE_URL}/sessions`, credentials);
+  res.user = res.data;
+  return res;
+  
+}
