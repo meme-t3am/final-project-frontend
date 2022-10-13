6e1f949b28715d1b171e26e9e0f58d3552f0ae90
‘AMAYA <aamaya547@gmail.com>
2022-10-13 11:48:03 -0700

    Added a fetch function

--- src/components/Services/request.js
+++ src/components/Services/request.js
@@ -1,1 +1,24 @@
 // dynamic handling of HTTP routes/endpoints
+
+async function doFetch(url, method, body) {
+  const options = {
+    method,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    mode: 'cors',
+  };
+
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+  const res = await fetch(url, options);
+  const resBody = await res.json();
+  const error = res.ok ? null : resBody;
+  const data = res.ok ? resBody : null;
+  return { data, error };
+}
+
+export const post = (url, body) => doFetch(url, 'POST', body);
