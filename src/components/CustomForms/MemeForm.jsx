bbe9a8d1496e9fcf2300407b1dbe8f2f620f8a77
‘AMAYA <aamaya547@gmail.com>
2022-10-17 11:49:20 -0700

    Added basic stylint to app.css and memeform.css

--- src/components/CustomForms/MemeForm.jsx
+++ src/components/CustomForms/MemeForm.jsx
@@ -1,27 +1,21 @@
 import { FormButton, InputControl } from './FormControl';
 import styles from './MemeForm.css';
 import '../App.css';
+import './MemeForm.css';
 
 export default function MemeForm() {
   return (
     <div className={styles.MemeForm}>
-      <h1>AllegorI</h1>
-      <div className={styles.InputForm}>
+      <h1>Allegori</h1>
+      <div className={styles.Input}>
         <form>
-          <InputControl
-            label="Upload Image URL"
-            type="text"
-          >
-          </InputControl>
+          <InputControl label="Upload Image URL" type="text"></InputControl>
 
           <FormButton>Upload</FormButton>
         </form>
       </div>
-      <div className={styles.Output}>
-      </div>
-      <div>
-        
-      </div>
+      <div className={styles.Output}></div>
+      <div></div>
     </div>
   );
 }
