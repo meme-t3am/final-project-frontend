3439d7874cba37de0a907189c93a29f4ff07df49
David <Rigby335@hotmail.com>
2022-10-18 09:46:03 -0700

    setup mock fetches and hooks

--- src/components/Services/meme.js
+++ src/components/Services/meme.js
@@ -1,3 +1,7 @@
+import { post } from './request.js';
+
+const BASE_URL = '/api/v1/imaggas';
+
 export function insertMeme() {
   const res = {
     url: 'https://i.kym-cdn.com/photos/images/newsfeed/000/911/486/6bb.jpg',
@@ -8,5 +12,13 @@ export function insertMeme() {
       },
     ],
   };
+  return res;
+}
+
+// will need to write a new endpoint POST /userImages, so when we post to front
+// it'll POST to /userImages
+
+export async function postToApi() {
+  const res = await post(`${BASE_URL}`);
   return res;
 }
